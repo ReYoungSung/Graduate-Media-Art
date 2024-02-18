5d971ed8b99e74756341ab58dacfcca9566b881e
max4779 <max4779@naver.com>
2024-02-18 23:45:53 +0900

    2024-02-18 수정
    매직완드: 좌표, 충돌 스크립트 추가
    의자 : 좌표 스크립트 추가
    게임 매니저: 씬 넘기기 기능(spacebar) 추가
    사운드매니저 : SFXStop함수 추가 ,오류는 당장은 발견못함
    임시 씬(Scene2) 추가. 이 씬은 아무것도 없음.

---
+++ GMA/Assets/MagicWandPosition.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicWandPosition : MonoBehaviour
+{
+    Vector3 vec;
+    float positionX,positionY, positionZ;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        vec = transform.position;
+        positionX = vec.x;
+        positionY = vec.y;
+        positionZ = vec.z;
+    }
+}
