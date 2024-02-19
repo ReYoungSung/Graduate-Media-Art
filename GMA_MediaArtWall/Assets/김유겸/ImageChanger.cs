4a8b79e655497871daceed922bb16bbd3284720f
max4779 <max4779@naver.com>
2024-02-20 05:15:39 +0900

    물결치는 스크린 1,2,3번으로 전환(어색함)
    물결치는 스크린 1,2,3번으로 전환(어색함)
    1,2,3번을 누르면 위치를 바꾸는 방식으로 생각중

--- GMA_MediaArtWall/Assets/김유겸/ImageChanger.cs
+++ GMA_MediaArtWall/Assets/김유겸/ImageChanger.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 using UnityEngine.VFX;
 
 public class ImageChanger : MonoBehaviour
-{
+{ 
     [SerializeField]
-    VisualEffectAsset VE_Asset;
     VisualEffectAsset VE_Asset_0;
     [SerializeField]
     VisualEffectAsset VE_Asset_1;
@@ -14,29 +13,36 @@ public class ImageChanger : MonoBehaviour
     VisualEffectAsset VE_Asset_2;
 
     VisualEffect VE;
+    VisualEffectAsset VE_Asset;
+    bool isRunning = false;
+    float runningTime = 0f;
 
 
     void Start()
     {
         VE=GetComponent<VisualEffect>();
+        VE_Asset = VE.visualEffectAsset;
     }
 
-    
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha0) && VE_Asset != null)
+
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            ChangeVE();
+            isRunning = true;
+            runningTime = 0f;
         }
-    }
+        else
+        {
+            
+        }
 
-    void ChangeVE()
-    {
-        VE.Stop();
 
-        VE.visualEffectAsset = VE_Asset;
 
-        VE.Play();
+
         if (Input.GetKeyDown(KeyCode.Alpha1) && VE_Asset_0 != VE.visualEffectAsset) //0누르면 변경
         {
             ChangeVE("1");
@@ -51,6 +57,16 @@ public class ImageChanger : MonoBehaviour
         {
             ChangeVE("3");
         }
+    }
+
+    void ChangeVE()
+    {
+        VE.Stop();
+
+        VE.visualEffectAsset = VE_Asset;
+
+        VE.Play();
+
     }
 
     void ChangeVE(string number)
@@ -69,6 +85,8 @@ public class ImageChanger : MonoBehaviour
         {
             VE.visualEffectAsset = VE_Asset_2;
         }
+
+
 
     }
 }
