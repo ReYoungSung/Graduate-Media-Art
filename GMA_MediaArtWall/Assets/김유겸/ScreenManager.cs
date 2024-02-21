b1b52bbe5777be5dbb98552b4c79bfe88865624a
Youngsung Ree <sjy06238@naver.com>
2024-02-21 10:07:01 +0900

    페이즈2 수정사항
    BGM추가, 영상 재생 가능한 부분 완성

--- GMA_MediaArtWall/Assets/김유겸/ScreenManager.cs
+++ GMA_MediaArtWall/Assets/김유겸/ScreenManager.cs
@@ -22,6 +22,7 @@ public class ScreenManager : MonoBehaviour
 
     string number = "";
     int count = 0;
+    int soundCount = 0;
     private float deltabox1X;
     private float deltabox1Z;
     float distance1=0, distance2=0, distance3=0;
@@ -71,12 +72,28 @@ public class ScreenManager : MonoBehaviour
                 if (distance1 >= 1)
                 {
                     VE1.SetFloat("SBSize1", 120 - (distance1 * 3));
-
-                }
+                    if ((20 < distance1 && distance1 < 21 && soundCount==0) || 
+                        (17 < distance1 && distance1 < 18 && soundCount == 1) || 
+                        (14 < distance1 && distance1 < 15 && soundCount == 2) || 
+                        (11 < distance1 && distance1 < 12 && soundCount == 3) ||
+                        (8 < distance1 && distance1 < 9 && soundCount == 4) ||
+                        (5 < distance1 && distance1 < 6 && soundCount == 5) ||
+                        (2 < distance1 && distance1 < 3 && soundCount == 6)
+                        )
+                    {
+                        SoundManager.instance.PlaySFX("WallBreak");
+                        SoundManager.instance.SetSFXVolume(50);
+                        soundCount++;
+                    }
+                 }
                 else
                 {
                     StartCameraMoving = true;
                     count++;
+                    soundCount = 0;
+                    SoundManager.instance.StopSFX("BusSound");
+                    SoundManager.instance.PlaySFX("BusStop");
+                    SoundManager.instance.PlaySFX("BusSound");
                 }
             }
 
@@ -85,10 +102,20 @@ public class ScreenManager : MonoBehaviour
                 if (distance2 >= 1)
                 {
                     VE2.SetFloat("SBSize2", 80- distance2 * 3);
+                    if ((14 < distance2 && distance2 < 15 && soundCount == 0)|| (12 < distance2 && distance2 < 13 && soundCount == 1))
+                    {
+                        SoundManager.instance.PlaySFX("WallBreak");
+                        SoundManager.instance.SetSFXVolume(50);
+                        soundCount++;
+                    }
                 }
                 else
                 {
                     count++;
+                    soundCount = 0;
+                    SoundManager.instance.StopSFX("BusSound");
+                    SoundManager.instance.PlaySFX("BusStop");
+                    SoundManager.instance.PlaySFX("BusSound");
                 }
             }
 
@@ -97,10 +124,19 @@ public class ScreenManager : MonoBehaviour
                 if (distance3 >= 1)
                 {
                     VE2.SetFloat("SBSize3", 80- distance3 * 3);
+                    if ((14 < distance3 && distance3 < 15 && soundCount == 0) || (12 < distance3 && distance3 < 13 && soundCount == 1))
+                    {
+                        SoundManager.instance.PlaySFX("WallBreak");
+                        SoundManager.instance.SetSFXVolume(50);
+                        soundCount++;
+                    }
                 }
                 else
                 {
                     count++;
+                    SoundManager.instance.StopSFX("BusSound");
+                    SoundManager.instance.PlaySFX("BusStop");
+                    SoundManager.instance.PlaySFX("BusSound");
                 }
             }
 
@@ -109,7 +145,9 @@ public class ScreenManager : MonoBehaviour
                 if (distance1 <= 1)
                 {
                     count++;
+                    SoundManager.instance.StopSFX("BusSound");
                     StartCoroutine(FallDown());
+
                 }
             }
 
@@ -118,13 +156,16 @@ public class ScreenManager : MonoBehaviour
 
     void screen1_move()
     {
-        //SoundManager.instance.PlaySFX();
+        
         float Y = screen1.transform.position.y - speed * Time.deltaTime;
         screen1.transform.position = new Vector3(screen1.transform.position.x, Y, screen1.transform.position.z);
 
         if (screen1.transform.position.y <= 0)
         {
             isMoving = false;
+            SoundManager.instance.PlayBGM("21",true);
+            SoundManager.instance.SetBGMVolume(1);
+            SoundManager.instance.PlaySFX("BusSound");
         }
     }
 
@@ -148,13 +189,18 @@ public class ScreenManager : MonoBehaviour
 
     IEnumerator FallDown()
     {
+        SoundManager.instance.StopBGM();
+        
         yield return new WaitForSeconds(3f);
+        SoundManager.instance.StopBGM();
+        SoundManager.instance.PlaySFX("CurtainCall");
         VE3.SetFloat("Gravity", -10);
         yield return new WaitForSeconds(2f);
         images.SetActive(false);
         screen2.SetActive(false);
         yield return new WaitForSeconds(1f);
         VE3.SetFloat("Gravity", 0);
+        
     }
 
    void AllScreenForward()
@@ -165,7 +211,7 @@ public class ScreenManager : MonoBehaviour
 
         float screen2_Z = screen2.transform.position.z - speed * Time.deltaTime * 20;
         screen2.transform.position = new Vector3(screen2.transform.position.x, screen2.transform.position.y, screen2_Z);
-
+        
         if (screen2_Z <= -1)
         {
             images.SetActive(true);
